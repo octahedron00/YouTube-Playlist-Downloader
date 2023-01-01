99989417841db2aad41a082dcbba201783bf549a
Octo Moon <75821497+octahedron00@users.noreply.github.com>
2023-01-02 04:29:16 +0900

    Update main.py
    Changing name after download, etc

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 from pytube import Playlist, YouTube
 import subprocess
 
@@ -9,26 +9,48 @@ print(p.title, "Length:", len(p.video_urls))
 for i in p.video_urls[:10]:
     print(i)
 
+# download_path here.
+download_path = "C:\\Python\\youtube"
+
 # Downloading
 for num, i in enumerate(p.video_urls):
     # use_oauth : for download YouTube_Music-only, if you have YouTube Music account.
     yt = YouTube(i, use_oauth=True)
 
     # to see what filter has the best quality : here
-    print("Filters : ", len(yt.streams.filter(only_audio=True)), yt.streams.filter(only_audio=True).order_by("abr"))
+    print("Filters :", len(yt.streams.filter(only_audio=True)), yt.streams.filter(only_audio=True).order_by("abr"))
 
     # the best quality : ~160kbps of webm, or the best of existing filters.
     audio = yt.streams.filter(only_audio=True).order_by("abr")[-1]
 
-    # download to the path, you can change this
-    out_file = audio.download(output_path="C:\\Python\\youtube")
+    # download to the path, you can change this.
+    out_file = audio.download(output_path=download_path)
 
     # change name to, base + " by " + author, without any error.
     base, ext = os.path.splitext(out_file)
     print(yt.author, base, ext)
-    forbidden_chars = '"*\\/\'|?:<>'
-    author = ''.join([x if x not in forbidden_chars else '#' for x in yt.author])
-    new = base + " by " + author
+
+    # replace any forbidden_chars in author name.
+    forbidden_chars = '*\\/|?:<>'
+    author = ''.join([x if x not in forbidden_chars else ' ' for x in yt.author])
+
+    # delete author name from video title.
+    author_list = re.split(" |\.|,|\[|]|\(|\)|-|\*|<|>|〔|〕|【|】|（|）|／|｢|｣|『|』|「|」|　", author)
+    base = str(base).replace(download_path+"\\", "")
+    for sentence in author_list:
+        base = base.replace(sentence, "")
+
+    # trim author name.
+    author.replace("- Topic", "")
+    author.replace("Official", "")
+    author.replace("Channel", "")
+    author.replace("YouTube", "")
+    author.strip()
+
+    # make new name(address) with author and song name
+    new = download_path + "\\" + author + " - " + base
+    new = new.strip()
+
     os.rename(out_file, new+".webm")
 
     # ADDITIONAL : change webm to 320kbps mp3, by using pre-installed ffmpeg
@@ -37,4 +59,28 @@ for num, i in enumerate(p.video_urls):
     print(subprocess.run(f'ffmpeg -i "{new}.webm" -codec:a libmp3lame -b:a 320k "{new}.mp3"', shell=True,
                          capture_output=True).stdout)
     print(f"{num+1}/{len(p.video_urls)} = {round((num+1)/len(p.video_urls)*100, 2)}% finished.")
+    os.remove(new+".webm")
     print("\n")
+
+
+file_list = os.listdir(download_path)
+
+for file in file_list:
+    original = file
+    file = file.replace("- Topic", "")
+    file = file.replace("YouTube", "")
+    file = file.replace("Video", "")
+    file = file.replace("MV", "")
+    file = file.replace("OFFICIAL", "")
+    file = file.replace("Official", "")
+    file = file.replace("Channel", "")
+    file = file.replace("Music ", "")
+
+    file = file.replace("　", "")
+    file = file.replace("  ", " ")
+    file = file.replace("  ", " ")
+    file = file.replace("–", "-")
+    file = file.replace("- -", "-")
+    file = file.replace("--", "")
+
+    os.rename(download_path+"\\"+original, download_path+"\\"+file)
